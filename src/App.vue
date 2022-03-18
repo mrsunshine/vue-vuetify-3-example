986a07229f1daef9521a4b878d6efa8dfed30b90
mrsunshine <mail@nils-dehl.de>
2022-03-18 17:03:27 +0100

    Add Users

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+      <Users />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Users from './components/Users.vue';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Users,
   },
 
   data: () => ({
     //
   }),
-}
+};
 </script>
---
+++ src/components/Users.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <h1>Users</h1>
+        <pre>{{users}}</pre>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Users',
+
+  data: () => ({
+    users: [],
+  }),
+  created() {
+    this.load();
+  },
+
+  methods: {
+    load() {
+      fetch('https://jsonplaceholder.typicode.com/users')
+        .then((response) => response.json())
+        .then((json) => (this.users = json));
+    },
+  },
+};
+</script>
